20160408ab03fc39f619d23488864cdee3a05dcc
Taki <prgrm2960.17chan@gmail.com>
2020-09-09 16:42:29 +0900

    add Project

---
+++ Assets/Script/LoadInputField/LoadInputField.cs
@@ -0,0 +1,155 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class LoadInputField : MonoBehaviour
+{
+    PlayerData a;
+
+
+    //string path = Application.persistentDataPath + "/";
+    //string fileName = Application.companyName + "." + Application.productName + ".savedata.json";
+
+    const string AES_KEY = "8sm9Kusy121Z95QQ";
+    const string AES_IV = "UA5498MjET4gao7Q";
+
+    [SerializeField] GameObject stringInput;
+    [SerializeField] GameObject intInput;
+    [SerializeField] GameObject floatInput;
+    [SerializeField] GameObject saveButton;
+    [SerializeField] GameObject loadButton;
+    [SerializeField] Text text;
+
+    [System.Serializable]
+    class PlayerData
+    {
+        public string name="なし";
+        public int money=0;
+        public float hight=0;
+        public int[] arrayNum = new int[10];
+    }
+
+
+
+
+
+    string PATH;
+    string FILE_NAME;
+
+
+    PlayerData playerData;
+    
+    private void Awake()
+    {
+        PATH = Application.persistentDataPath + "/";
+        FILE_NAME = Application.companyName + "." + Application.productName + ".GamaData";
+
+        playerData = new PlayerData();
+        stringInput.GetComponent<InputField>().onEndEdit.AddListener((a) => SaveString());
+        floatInput.GetComponent<InputField>().onEndEdit.AddListener((a) => SaveFloat());
+        intInput.GetComponent<InputField>().onEndEdit.AddListener((a) => SaveInt());
+        saveButton.GetComponent<Button>().onClick.AddListener(() => SaveData.SavePlayerData(playerData, PATH + FILE_NAME, AES_IV, AES_KEY));
+        loadButton.GetComponent<Button>().onClick.AddListener(() => SaveData.LoadPlayerData(ref playerData, PATH + FILE_NAME, AES_IV, AES_KEY));
+
+        test2();
+
+    }
+
+    void Update()
+    {
+        text.text = "名前は" + playerData.name + "\nお金は" + playerData.money + "\n高さは" + playerData.hight;
+    }
+
+
+    void SaveString()
+    {
+        InputField inputField = stringInput.GetComponent<InputField>();
+        if(inputField.text != null)
+        {
+            playerData.name = inputField.text;
+        }
+
+    }
+
+    void SaveInt()
+    {
+        InputField inputField = intInput.GetComponent<InputField>();
+        int num;
+        if (int.TryParse(inputField.text,out num))
+        {
+            playerData.money = num;
+        }
+
+    }
+
+    void SaveFloat()
+    {
+        InputField inputField = floatInput.GetComponent<InputField>();
+        float num;
+        if (float.TryParse(inputField.text, out num))
+        {
+            playerData.hight= num;
+        }
+
+    }
+
+
+
+    void test()
+    {
+        try
+        {
+            a = new PlayerData();
+            Debug.Log(JsonUtility.ToJson(a));
+            a = JsonUtility.FromJson<PlayerData>("{ \"name\":\"最強\",\"money\":19292,\"hight\":999.666}");
+        }
+        catch
+        {
+            Debug.Log("huseinaSaveData");
+
+        }
+        Debug.Log(a.name + a.money + a.hight);
+    }
+
+    void test2()
+    {
+        string iv = "pppppppppppppppp";
+        string key = "cccccccccccccccc";
+        try
+        {
+            a = new PlayerData();
+            PlayerData b = new PlayerData();
+
+            a.name = "配列君";
+            a.money = 6666;
+            a.hight = 161.2f;
+            for (int i = 0; i < 10; i++)
+            {
+                a.arrayNum[i] = i + 1;
+            }
+
+            SaveData.SavePlayerData<PlayerData>(a, PATH + "TestSaveData", iv,key);
+
+            SaveData.LoadPlayerData<PlayerData>(ref b, PATH + "TestSaveData", iv,key);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.Log(b.arrayNum[i]);
+            }
+
+
+        }
+        catch
+        {
+            Debug.Log("huseinaSaveData");
+
+        }
+        Debug.Log(a.name + a.money + a.hight);
+    }
+
+
+}
---
+++ Assets/Script/SaveData.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+public class SaveData 
+{
+    static public void SavePlayerData<ClassType>(ClassType instance, string path, string aesIV, string aesKey)
+    {
+        string jsonData = JsonUtility.ToJson(instance);//セーブしたいクラスをJsonに変換
+
+        string cipher = Encrypt(jsonData, aesIV, aesKey);//暗号化
+
+        using (StreamWriter writer = new StreamWriter(path, false, Encoding.GetEncoding("utf-8")))
+        {
+            writer.WriteLine(cipher);//書き込む
+        }
+    }
+
+
+    static public void LoadPlayerData<ClassType>(ref ClassType instance, string path, string aesIV, string aesKey)
+    {
+        if (!File.Exists(path))//ファイルが存在しないなら早期リターン
+        {
+            Debug.Log("ファイルは存在しません");
+            return;
+        }
+
+        string cipher;
+        using (StreamReader sr = new StreamReader(path, Encoding.GetEncoding("utf-8")))
+        {
+            cipher = sr.ReadToEnd();//暗号文の読み込み
+        }
+
+        string plain = Decrypt(cipher, aesIV, aesKey);//復号
+
+        try
+        {
+            instance = JsonUtility.FromJson<ClassType>(plain);//ref したクラスのインスタンスに情報を格納する
+        }
+        catch
+        {
+            Debug.Log("不正なセーブデータ、あるいはバグです");
+        }
+
+
+        //Debug.Log(playerData);
+    }
+
+
+
+
+    //==================================================================================
+    //以下、https://qiita.com/kz-rv04/items/62a56bd4cd149e36ca70 のスクリプトを使用
+    //==================================================================================
+
+    /// <summary>
+    /// 対称鍵暗号を使って文字列を暗号化する
+    /// </summary>
+    /// <param name="text">暗号化する文字列</param>
+    /// <param name="iv">対称アルゴリズムの初期ベクター</param>
+    /// <param name="key">対称アルゴリズムの共有鍵</param>
+    /// <returns>暗号化された文字列</returns>
+    public static string Encrypt(string text, string iv, string key)
+    {
+
+        using (RijndaelManaged rijndael = new RijndaelManaged())
+        {
+            rijndael.BlockSize = 128;
+            rijndael.KeySize = 128;
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
+
+            rijndael.IV = Encoding.UTF8.GetBytes(iv);
+            rijndael.Key = Encoding.UTF8.GetBytes(key);
+
+            ICryptoTransform encryptor = rijndael.CreateEncryptor(rijndael.Key, rijndael.IV);
+
+            byte[] encrypted;
+            using (MemoryStream mStream = new MemoryStream())
+            {
+                using (CryptoStream ctStream = new CryptoStream(mStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(ctStream))
+                    {
+                        sw.Write(text);
+                    }
+                    encrypted = mStream.ToArray();
+                }
+            }
+            return (System.Convert.ToBase64String(encrypted));
+        }
+    }
+
+    /// <summary>
+    /// 対称鍵暗号を使って暗号文を復号する
+    /// </summary>
+    /// <param name="cipher">暗号化された文字列</param>
+    /// <param name="iv">対称アルゴリズムの初期ベクター</param>
+    /// <param name="key">対称アルゴリズムの共有鍵</param>
+    /// <returns>復号された文字列</returns>
+    public static string Decrypt(string cipher, string iv, string key)
+    {
+        using (RijndaelManaged rijndael = new RijndaelManaged())
+        {
+            rijndael.BlockSize = 128;
+            rijndael.KeySize = 128;
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
+
+            rijndael.IV = Encoding.UTF8.GetBytes(iv);
+            rijndael.Key = Encoding.UTF8.GetBytes(key);
+
+            ICryptoTransform decryptor = rijndael.CreateDecryptor(rijndael.Key, rijndael.IV);
+
+            string plain = string.Empty;
+            using (MemoryStream mStream = new MemoryStream(System.Convert.FromBase64String(cipher)))
+            {
+                using (CryptoStream ctStream = new CryptoStream(mStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(ctStream))
+                    {
+                        plain = sr.ReadLine();
+                    }
+                }
+            }
+            return plain;
+        }
+    }
+}
